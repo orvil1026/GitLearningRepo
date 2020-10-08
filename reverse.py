c2dc90877eb9712fa0392a4dfaac76d3db021fb1
orvil1026 <58859056+orvil1026@users.noreply.github.com>
2020-10-08 21:33:56 +0530

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,12 @@
+txt = "Hello World"[::-1]
+print(txt)
+
+txt = "Hello World" [::-1]
+print(txt)
+
+def my_function(x):
+  return x[::-1]
+
+mytxt = my_function("I wonder how this text looks like backwards")
+
+print(mytxt)
