5ac3b5ab6f9129cdc1072525940054b1bbd169af
orvil1026 <58859056+orvil1026@users.noreply.github.com>
2020-10-08 22:43:22 +0530

    Create StringDemo.java

---
+++ StringDemo.java
@@ -0,0 +1,20 @@
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String s1="hello";
+		String s2="how";
+		String s3="hello";
+		
+		
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s1.concat(s2));
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.replace('l','p'));
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.substring(3));
+		System.out.println(s1);
+		System.out.println(s2);
+		
+		}
+
+}
